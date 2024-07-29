141425718db80babad7e39bda5a45395a8323198
Harouna Kinda <harounakinda@gmail.com>
2024-07-29 17:59:31 +0000

    get users which make test OK

--- src/routes/getAllUsers.js
+++ src/routes/getAllUsers.js
@@ -18,14 +18,27 @@ module.exports = (app) => {
         res.json({ message, data: users });
       });
     }
+
+    if (req.query.temperamentIsNul === "true") {
+      return User.findAll({
+        where: {
+          temperament: {
+            [Op.is]: null,
+          },
+        },
+      }).then((users) => {
+        const message = `Les utilisateurs dont le tempérament est nul sont au nombre de ${users.length}`;
+        res.json({ message, data: users });
+      });
+    }
+
     User.findAll()
       .then((users) => {
-        const message = `La liste complète des utilisateurs a bien été reccupérée.`;
-
+        const message = `La liste complète des utilisateurs a bien été récupérée.`;
         res.json({ message, data: users });
       })
       .catch((error) => {
-        const message = `La liste complète des utilisateurs n'a pas pu être reccupéré. Réessayer dans quelques instants.`;
+        const message = `La liste complète des utilisateurs n'a pas pu être récupérée. Réessayez dans quelques instants.`;
         res.status(500).json({ message, data: error });
       });
   });
